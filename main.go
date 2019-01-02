6db9febca856125a646cdbe9e5a6bcab49a7f3d1
Fang Shuo Deng <dfs.arthur@berkeley.edu>
2019-01-02 16:02:13 +0800

    read and write endpoints

--- main.go
+++ main.go
@@ -1,137 +1,132 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"firebase.google.com/go/db"
 	"github.com/dfangshuo/mmModels"
 	"github.com/gorilla/mux"
-	"log"       // log errors and stuff
-	"math/rand" // specific to tutorial: generate random num
-	"net/http"  // work with http
-	"strconv"   // str converter
+	"log"      // log errors and stuff
+	"net/http" // work with http
+	// "strconv"  // str converter
+	"github.com/kjk/betterguid"
+
+	firebase "firebase.google.com/go"
+	// "firebase.google.com/go/auth"
+	"google.golang.org/api/option"
 )
 
 // Init books var as a slice Book struct
-var books []mmModels.Book
+var users []mmModels.MeetMeUser
+var port = ":8000"
+var client *db.Client
+var userRef *db.Ref
+var ctx context.Context
+var router *mux.Router
 
-// get all books
-func getBooks(w http.ResponseWriter, r *http.Request) {
-
-	/*
-		Let's take a step back and try to see what's happening here:
-		your r is your REQUEST
-		your w is your RESPONSE
-		This is saying, firstly, SET (in the header of your response) that you're sending back at JSON
-		ok. This is so you won't be serving content as txt
-		THEN, you want to create a new encder that WRITES TO w (your response)
-		And specifically, you want to write your BOOKS to your response
-	*/
-
+// get all users
+func getUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
-	// .NewEncoder returns a NewEncoder that writes to w
-	// Encode writes the JSON encoding of v (books) into the stream (the new Encoder returned )
+	var users []mmModels.MeetMeUser
+	if err := userRef.Get(ctx, &users); err != nil {
+		log.Fatalf("getUsers error | %v\n", err)
+	}
+	json.NewEncoder(w).Encode(users)
 }
 
-// get 1 book
-func getBook(w http.ResponseWriter, r *http.Request) {
-	//parse id from r
+// HELPER function to get 1 user by ID
+func getUserByID(id string) mmModels.MeetMeUser {
+	var user mmModels.MeetMeUser
+	if err := userRef.Child(id).Get(ctx, &user); err != nil {
+		log.Fatalf("getUserByID error | %v\n", err)
+	}
+	return user
+}
+
+func getOneUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) // get any params
-	for _, book := range books {
-		if book.Id == params["id"] {
-			json.NewEncoder(w).Encode(book)
-			return
-		}
-	}
-	// empty book for book not found
-	json.NewEncoder(w).Encode(&mmModels.Book{Title: "ERROR: book not found!"})
-
+	user := getUserByID(params["id"])
+	json.NewEncoder(w).Encode(user)
 }
 
 // create a new book
-func createBook(w http.ResponseWriter, r *http.Request) {
+func createUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var newBook mmModels.Book
+	var newUser mmModels.MeetMeUser
+	_ = json.NewDecoder(r.Body).Decode(&newUser)
+	newID := betterguid.New()
 
-	/*
-		NewDecoder creates a NewDecoder that READS from the source (r.Body, in this case)
-		Decode reads the next JSON-encoded value from its input and
-		stores it in the value pointed to by v (which in this case, is &newBook)
-	*/
-	_ = json.NewDecoder(r.Body).Decode(&newBook)
-	newBook.Id = strconv.Itoa(rand.Intn(100000000)) // not safe for prod - overlapping Ids
-	// adds the book created to the 'database'
-	books = append(books, newBook)
-	// returns the new book created
-	json.NewEncoder(w).Encode(newBook)
-
-}
-
-// update book
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // get any params
-	for i, book := range books {
-		if book.Id == params["id"] {
-			books = append(books[:i], books[i+1:]...)
-			var newBook mmModels.Book
-			_ = json.NewDecoder(r.Body).Decode(&newBook)
-			newBook.Id = params["id"]
-			books = append(books, newBook)
-			json.NewEncoder(w).Encode(newBook)
-			return
-		}
+	for getUserByID(newID).ID != "" {
+		newID = betterguid.New()
 	}
-	// remove that book
-	json.NewEncoder(w).Encode(books)
+
+	newUser.ID = newID
+
+	if err := userRef.Child(newID).Set(ctx, newUser); err != nil {
+		log.Fatalf("ref error, %v", err)
+	}
+
+	json.NewEncoder(w).Encode(newUser)
+
 }
 
-// delete book
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // get any params
-	for i, book := range books {
-		if book.Id == params["id"] {
-			books = append(books[:i], books[i+1:]...)
-			break
-		}
-	}
-	// remove that book
-	json.NewEncoder(w).Encode(books)
-}
+// // update book
+// func updateBook(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	params := mux.Vars(r) // get any params
+// 	for i, book := range books {
+// 		if book.Id == params["id"] {
+// 			books = append(books[:i], books[i+1:]...)
+// 			var newBook mmModels.Book
+// 			_ = json.NewDecoder(r.Body).Decode(&newBook)
+// 			newBook.Id = params["id"]
+// 			books = append(books, newBook)
+// 			json.NewEncoder(w).Encode(newBook)
+// 			return
+// 		}
+// 	}
+// 	// remove that book
+// 	json.NewEncoder(w).Encode(books)
+// }
+
+// // delete book
+// func deleteBook(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	params := mux.Vars(r) // get any params
+// 	for i, book := range books {
+// 		if book.Id == params["id"] {
+// 			books = append(books[:i], books[i+1:]...)
+// 			break
+// 		}
+// 	}
+// 	// remove that book
+// 	json.NewEncoder(w).Encode(books)
+// }
 
 func main() {
-	// init router
-	r := mux.NewRouter()
+	ctx = context.Background()
+	config := &firebase.Config{
+		DatabaseURL: "https://gotest-76eff.firebaseio.com/",
+	}
+	opt := option.WithCredentialsFile("gotest-76eff-firebase-adminsdk-pjcks-795d67e1db.json")
+	app, err := firebase.NewApp(ctx, config, opt)
+	if err != nil {
+		log.Fatalf("app initialization failure | %v", err)
+	}
+	client, err = app.Database(ctx)
+	if err != nil {
+		log.Fatalf("client initialization failure | %v", err)
+	}
+	userRef = client.NewRef("Users")
 
-	// Mock Data
-	// TODO: implement database
-	books = append(books, mmModels.Book{
-		Id:    "1",
-		Isbn:  "94704",
-		Title: "Book One",
-		Author: &mmModels.Author{
-			Firstname: "John",
-			Lastname:  "W",
-		},
-	})
-
-	books = append(books, mmModels.Book{
-		Id:    "2",
-		Isbn:  "21421",
-		Title: "Book Two",
-		Author: &mmModels.Author{
-			Firstname: "Steve",
-			Lastname:  "Smith",
-		},
-	})
-
+	router = mux.NewRouter()
 	// Route handlers/ endpoints
-	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/books", createBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	// router.HandleFunc("/api/users", getUsers).Methods("GET")
+	router.HandleFunc("/api/users/{id}", getOneUser).Methods("GET")
+	router.HandleFunc("/api/users", createUser).Methods("POST")
+	// r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	// r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(port, router))
 }
